4bbbf8836e8b4acd3faf4bad1b2be907f32fbd94
0djentd <58089540+0djentd@users.noreply.github.com>
2022-07-02 10:15:46 +0300

    modified:   nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["@/assets/style"],
 
+  router: {
+    base: "/portfolio/",
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
@@ -78,7 +82,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-      publicPath: "/portfolio/",
-  },
+  build: {},
 };
